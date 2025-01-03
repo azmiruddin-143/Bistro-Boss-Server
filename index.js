0f35310687f0de74c723bf13153b3e207a472402
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-01-03 15:32:50 +0600

    card data show and dilete function complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 app.use(cors())
 app.use(express.json())
@@ -28,17 +28,37 @@ async function run() {
     const database = client.db("restaurant");
     const menuCollection = database.collection("menu");
     const reviewCollection = database.collection("review");
+    const cartsCollection = database.collection("carts");
 
-    app.get('/menu', async (req,res) =>{
-        const result = await menuCollection.find().toArray();
-        res.send(result)
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
     })
-       
-    app.get('/review', async (req,res) =>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
+
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
     })
-       
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email
+      const query = {email : email}
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/carts', async (req, res) => {
+      const cartsBody = req.body
+      const result = await cartsCollection.insertOne(cartsBody);
+      res.send(result)
+    })
+    app.delete('/carts/:id', async (req, res) => {
+      const id  = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    
 
 
     // Send a ping to confirm a successful connection
@@ -52,11 +72,11 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
   res.send('Restrurand Website...')
 })
 
-app.listen(port,(req,res) =>{
-    console.log('server coltace',port);
+app.listen(port, (req, res) => {
+  console.log('server coltace', port);
 })
 
